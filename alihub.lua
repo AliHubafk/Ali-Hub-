389aeae66ad3939ef033ac0a302b6aaa58ee4446
AliHubafk <rodionovv838@gmail.com>
2025-08-19 17:36:44 +0300

    Create alihub.lua

---
+++ alihub.lua
@@ -0,0 +1,27 @@
+local Window = Library.CreateLib("Ali Hub", "RJTheme3")
+
+local Tab = Window:NewTab("Main")
+
+local Section = Tab:NewSection("Speed hack")
+
+Section:NewToggle("Speed hack", "ToggleInfo", function(state)
+    if state then
+        game.Players.LocalPlayer.Character.Humanoid.Walkspeed = 100
+    else
+        game.Players.LocalPlayer.Character.Humanoid.Walkspeed = 10
+    end
+end)
+
+Section:NewSlider("SliderText", "SliderInfo", 100, 10, function(s) -- 100 (Макс. значение) | 0 (Мин. значение)
+    game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = s
+end)
+
+local Section = Tab:NewSection("Jump hack")
+
+Section:NewToggle("Jump hack", "ToggleInfo", function(state)
+    if state then
+        game.Players.LocalPlayer.Character.Humanoid.JumpPower = 50
+    else
+        game.Players.LocalPlayer.Character.Humanoid.JumpPower = 10
+    end
+end)
